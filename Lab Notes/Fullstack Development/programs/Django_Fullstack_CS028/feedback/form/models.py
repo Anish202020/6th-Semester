ffd3f4c6e1c99fed0cfc61d0e48af622717eda01
Anish202020 <anishkumarbdmi@gmail.com>
2024-07-02 09:16:41 +0530

    commit changes

--- Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/feedback/form/models.py
+++ Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/feedback/form/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+
+    def _str_(self):
+        return self.name
+
+
+class Feedback(models.Model):
+    customer_name = models.CharField(max_length=120)
+    email = models.EmailField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    details = models.TextField()
+    happy = models.BooleanField()
+    date = models.DateField(auto_now_add=True)
+
+    def _str_(self):
+        return self.customer_name
