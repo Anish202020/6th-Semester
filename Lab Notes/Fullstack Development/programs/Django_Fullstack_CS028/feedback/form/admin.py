cb63cf998e2fd47db200fec672a6a0d9598e4568
Anish202020 <anishkumarbdmi@gmail.com>
2024-07-02 09:33:09 +0530

    commit changes

--- Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/feedback/form/admin.py
+++ Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/feedback/form/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Feedback
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('product', 'customer_name', 'date', 'happy',)
+    list_filter = ('product', 'date',)
+    search_fields = ('product__name', 'details',)
+
+    class Meta:
+        model = Feedback
+
+
+admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(Product)
