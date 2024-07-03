16d5b4ef01878d708cb0bd58e4ad064b97c4c403
Anish202020 <anishkumarbdmi@gmail.com>
2024-07-03 08:57:02 +0530

    commit changes

---
+++ Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/feedback/form/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path,include
+from .views import FeedbackForm
+urlpatterns=[
+    path('/feedback',)        
+]
+
--- Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/feedback/form/views.py
+++ Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/feedback/form/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import FeedbackForm
+
+
+def feedback_form(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return render(request, 'form/thanks.html')
+    else:
+        form = FeedbackForm()
+    return render(request, 'form/feedback_form.html', {'form': form})
