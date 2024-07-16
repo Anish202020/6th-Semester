717a271c1691d6da4273988885e3ef5a22bcf091
Anish202020 <anishkumarbdmi@gmail.com>
2024-07-16 09:07:16 +0530

    commit changes

--- Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/class_generic_project/class_generic_app/models.py
+++ Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/class_generic_project/class_generic_app/models.py
@@ -1,3 +1,12 @@
-from django.db import models
-
-# Create your models here.
+from django.db import models  
+  
+# Create your models here.  
+  
+class Employee(models.Model):  
+    first_name = models.CharField(max_length=30)  
+    last_name = models.CharField(max_length=30)  
+    mobile = models.CharField(max_length=10)  
+    email = models.EmailField()  
+  
+    def _str_(self):  
+        return "%s %s" % (self.first_name, self.last_name)
