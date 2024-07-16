04959cea4df862855287219cb09432df5a4d7520
Anish202020 <anishkumarbdmi@gmail.com>
2024-07-16 09:17:10 +0530

    commit changes

---
+++ Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/class_generic_project/class_generic_app/forms.py
@@ -0,0 +1,11 @@
+from django.forms import fields  
+from .models import Employee  
+from django import forms  
+  
+class EmployeeForm(forms.ModelForm):  
+  
+    class Meta:  
+        # To specify the model to be used to create form  
+        model = Employee  
+        # It includes all the fields of model  
+        fields = '_all_'
--- Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/class_generic_project/class_generic_app/views.py
+++ Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/class_generic_project/class_generic_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse  
+from django.views import View  
+class NewView(View):  
+    def get(self, request):  
+        # View logic will place here  
+        return HttpResponse('response')
