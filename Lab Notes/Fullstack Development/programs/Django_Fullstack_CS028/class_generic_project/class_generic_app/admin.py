c8a3d084ea05136036ae78c15143fe9885f556f1
Anish202020 <anishkumarbdmi@gmail.com>
2024-07-17 09:37:06 +0530

    commit changes

--- Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/class_generic_project/class_generic_app/admin.py
+++ Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/class_generic_project/class_generic_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Employee
 
-# Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'mobile', 'email')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('first_name', 'last_name')
+
+admin.site.register(Employee, EmployeeAdmin)
