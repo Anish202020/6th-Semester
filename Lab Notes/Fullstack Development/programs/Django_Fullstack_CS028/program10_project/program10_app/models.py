38f74745e730684c42eddd107722992ee2d7a7b6
Anish202020 <anishkumarbdmi@gmail.com>
2024-07-18 11:18:58 +0530

    commit changes

--- Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/program10_project/program10_app/models.py
+++ Lab Notes/Fullstack Development/programs/Django_Fullstack_CS028/program10_project/program10_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=255, default='')
+
+class Project(models.Model):
+    title = models.CharField(max_length=255, default='')
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=255, default='')
+    last_name = models.CharField(max_length=255, default='')
+    email = models.EmailField(unique=True, default='')
+    courses = models.ManyToManyField(Course, related_name='students', blank=True)
+    project = models.ManyToManyField(Project, related_name='students', blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
