9d8276d8cad21cbf1516ec33572ae4e094602436
Anish202020 <anishkumarbdmi@gmail.com>
2024-07-21 18:56:46 +0530

    commit changes

---
+++ Lab IA Practice/Computer Graphics/practice1/program12/program12.py
@@ -0,0 +1,18 @@
+import cv2
+
+image = cv2.imread("photo_emotions.jpg")
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_eye.xml')
+
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray,scaleFactor=1.3,minNeighbors=5)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+    
+cv2.imwrite('Detected_Faces.jpg',image)
+cv2.imshow('Detected Faces',image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
