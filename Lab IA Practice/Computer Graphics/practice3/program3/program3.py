13995b17aed69cd1ceff17e9f0dc2512de925885
Anish202020 <anishkumarbdmi@gmail.com>
2024-08-01 17:40:23 +0530

    commit changes

--- Lab IA Practice/Computer Graphics/practice3/program3/program3.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt 
-
-def plot_3d_object(verticles,title):
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    ax.set_title(title)
-    
-    ax.scatter(verticles[:,0],verticles[:,1],verticles[:,2],color='b')
